e4b553bfa33b967e06d4b19d2e108cc31903914f
sheminghao <469150981@qq.com>
2016-12-22 23:56:31 +0800

    .

--- app/src/main/java/cn/lzh/baby/ui/MainActivity.java
+++ app/src/main/java/cn/lzh/baby/ui/MainActivity.java
@@ -7,11 +7,12 @@ import org.xutils.view.annotation.ContentView;
 import cn.lzh.baby.R;
 import cn.lzh.baby.base.BaseActivity;
 
-@ContentView(R.layout.activity_main)
+//@ContentView(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
